536fefa4a961d9da502c0527bdd108ea4da8c2cf
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-01-19 11:42:48 +0600

    JPA project init commit

---
+++ learnjpaandhibernate/src/main/java/com/example/springboot/learnjpaandhibernate/LearnjpaandhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.learnjpaandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnjpaandhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnjpaandhibernateApplication.class, args);
+	}
+
+}
